b78bdb77f946ccdaae4bb8b9ff0a6e8c0efc16db
Peder Notto Galteland <peder.galteland@softwarelab.no>
2026-01-15 19:42:13 +0100

    Set nixos sway scaling

--- nixos/sway.nix
+++ nixos/sway.nix
@@ -1,6 +1,11 @@
 _: {
   wayland.windowManager.sway = {
     config = {
+      output = {
+        "eDP-1" = {
+          scale = "1.2";
+        };
+      };
       input = {
         "2:7:SynPS/2_Synaptics_TouchPad" = {
           dwt = "enabled";
