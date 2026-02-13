5351fcdf356926f5d2251a726cad283a89d73ebe
Peder Notto Galteland <peder.galteland@softwarelab.no>
2026-02-13 07:35:30 +0100

    Use jj in starship instead of git

--- common/terminal.nix
+++ common/terminal.nix
@@ -102,6 +102,63 @@
           format = "[$context](blue)[\(:$namespace\)](dimmed green) ";
         };
         aws = {disabled = true;};
+
+        git_status = {disabled = true;};
+        git_commit = {disabled = true;};
+        git_metrics = {disabled = true;};
+        git_branch = {disabled = true;};
+
+        custom = {
+          jj = {
+            description = "The current jj status";
+            when = "jj --ignore-working-copy root";
+            symbol = "🥋 ";
+            command = ''
+              jj log --revisions @ --no-graph --ignore-working-copy --color always --limit 1 --template '
+                separate(" ",
+                  change_id.shortest(4),
+                  bookmarks,
+                  "|",
+                  concat(
+                    if(conflict, "💥"),
+                    if(divergent, "🚧"),
+                    if(hidden, "👻"),
+                    if(immutable, "🔒"),
+                  ),
+                  raw_escape_sequence("\x1b[1;32m") ++ if(empty, "(empty)"),
+                  raw_escape_sequence("\x1b[1;32m") ++ coalesce(
+                    truncate_end(29, description.first_line(), "…"),
+                    "(no description set)",
+                  ) ++ raw_escape_sequence("\x1b[0m"),
+                )
+              '
+            '';
+          };
+          git_status = {
+            when = "! jj --ignore-working-copy root";
+            command = "starship module git_status";
+            style = "";
+            description = "Only show git_status if we're not in a jj repo";
+          };
+          git_commit = {
+            when = "! jj --ignore-working-copy root";
+            command = "starship module git_commit";
+            style = "";
+            description = "Only show git_commit if we're not in a jj repo";
+          };
+          git_metrics = {
+            when = "! jj --ignore-working-copy root";
+            command = "starship module git_metrics";
+            style = "";
+            description = "Only show git_metrics if we're not in a jj repo";
+          };
+          git_branch = {
+            when = "! jj --ignore-working-copy root";
+            command = "starship module git_branch";
+            style = "";
+            description = "Only show git_branch if we're not in a jj repo";
+          };
+        };
       };
     };
   };
