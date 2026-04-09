349dc95c88d7d4bee67523a5fbd8d698cf3ba3a2
Peder Notto Galteland <peder.galteland@softwarelab.no>
2026-04-09 07:19:27 +0200

    Remove no-longer needed nvidia patch on heisenberg

--- heisenberg/configuration.nix
+++ heisenberg/configuration.nix
@@ -23,26 +23,6 @@
       open = true;
       nvidiaSettings = true;
       modesetting.enable = true;
-      # Apply CachyOS kernel 6.19 patch to NVIDIA latest driver
-      package = let
-        base = config.boot.kernelPackages.nvidiaPackages.latest;
-        cachyos-nvidia-patch = pkgs.fetchpatch {
-          url = "https://raw.githubusercontent.com/CachyOS/CachyOS-PKGBUILDS/master/nvidia/nvidia-utils/kernel-6.19.patch";
-          sha256 = "sha256-YuJjSUXE6jYSuZySYGnWSNG5sfVei7vvxDcHx3K+IN4=";
-        };
-
-        # Patch the appropriate driver based on config.hardware.nvidia.open
-        driverAttr =
-          if config.hardware.nvidia.open
-          then "open"
-          else "bin";
-      in
-        base
-        // {
-          ${driverAttr} = base.${driverAttr}.overrideAttrs (oldAttrs: {
-            patches = (oldAttrs.patches or []) ++ [cachyos-nvidia-patch];
-          });
-        };
     };
     bluetooth = {
       enable = true;
